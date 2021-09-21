2dc806a1d896d9497f0cd79634a81cac06b84a43
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-09-21 13:46:57 +0900

    update proposals_alma.py

--- proposals_alma.py
+++ proposals_alma.py
@@ -213,7 +213,7 @@ class ProposalsALMA():
         	this_name = list_linename[i]
         	this_freq = list_linefreq[i]
         	ax1.plot([this_freq,this_freq],[1.0,6.0],color="green",lw=2)
-        	this_text = ax1.text(this_freq,6.0,this_name,rotation=30)
+        	this_text = ax1.text(this_freq,6.0,this_name,rotation=60,fontsize=11,ha="left",va="bottom")
 
         # ax2: arcival#spw
         for i in range(len(list_b3data)):
