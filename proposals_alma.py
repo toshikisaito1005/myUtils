358b5c9a03b34b7132cab99a2a45c74b2f529be3
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-09-21 17:27:26 +0900

    update proposals_alma.py

--- proposals_alma.py
+++ proposals_alma.py
@@ -211,6 +211,7 @@ class ProposalsALMA():
 
         width = abs(x[1] - x[0])
         ax.bar(x, y, lw=0, color="black", alpha=0.2, width=width, align="center")
+        print(x,y)
 
         ax.set_title("CO(1-0) missing flux histogram")
         ax.set_xlabel("missing flux (%)")
