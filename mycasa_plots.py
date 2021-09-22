f3fe6a7ad6ef67791a259f3ad199b026360fd903
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-09-22 13:08:23 +0900

    update proposals_alma.py

--- mycasa_plots.py
+++ mycasa_plots.py
@@ -850,12 +850,19 @@ def myax_fig2png_ann(ax,number,add_text=True):
     ###########################################
     if number==3:
         # plot CND outer radius
-        cnd_radius = 4.0 # racsec
+        cnd_radius = 2.0 # racsec
         e_cnd = patches.Ellipse(xy=(-0,0), width=fov_diamter,
             height=fov_diamter, angle=0, fill=False, edgecolor="black",
-            alpha=1.0, lw=3.5, ls="dashed")
+            alpha=1.0, lw=2.5, ls="dashed")
 
         ax.add_patch(e_cnd)
+
+        cnd_radius = 18.0 # racsec
+        e_sbr = patches.Ellipse(xy=(-0,0), width=fov_diamter,
+            height=fov_diamter, angle=0, fill=False, edgecolor="black",
+            alpha=1.0, lw=2.5, ls="dashed")
+
+        ax.add_patch(e_sbr)
 
 def _myax_comment(ax,dec_cnt,xlim,ylim,comment_color):
     if float(dec_cnt.replace("deg",""))>0:
