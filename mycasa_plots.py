a92ec761eb3b4c11daf5decc4e99b767df3b9a88
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-09-24 16:11:34 +0900

    update proposals_alma.py

--- mycasa_plots.py
+++ mycasa_plots.py
@@ -960,7 +960,7 @@ def myax_fig2png_ann(ax,number,ra_cnt,dec_cnt,add_text=True,txtfiles=None):
                 thisx = (float(ra_cnt.split("deg")[0]) - ra_dgr) * 3600.
                 thisy = (float(dec_cnt.split("deg")[0]) - dec_dgr) * 3600.
 
-                this_e = patches.Ellipse(xy=(thisx,ーthisy), width=b10_size,
+                this_e = patches.Ellipse(xy=(thisx,-thisy), width=b10_size,
                     height=b10_size, angle=0, fill=False, edgecolor="grey",
                     alpha=1.0, lw=1.0)
 
