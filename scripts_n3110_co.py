23bc7f115941fb4bafac9897d16259066b9dca10
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-10-07 17:10:28 +0900

    update escripts_n3110_co.py

--- scripts_n3110_co.py
+++ scripts_n3110_co.py
@@ -422,6 +422,7 @@ class ToolsNGC3110():
         ax.set_aspect('equal', adjustable='box')
 
         print(index[index!=0])
+        print(sfe_fix[index!=0])
 
         cax = ax.scatter(index, sfe_fix, s=100, c=dist_kpc, cmap="rainbow_r", linewidths=0, alpha=0.7,zorder=1e9)
         for i in range(len(dh2_fix)):
