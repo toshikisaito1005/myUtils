c43fc35bc52a06743b4ccd425d0ceabdaf04e5f2
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-10-07 17:03:43 +0900

    update escripts_n3110_co.py

--- scripts_n3110_co.py
+++ scripts_n3110_co.py
@@ -314,8 +314,8 @@ class ToolsNGC3110():
         sfe_vary     = np.log10(sfe * aco_lte_trot/aco_fix)
         sfe_err_vary = 0.3
 
-        index        = np.log10(index)
-        index_err    = 1/np.log(10) * err_index/index
+        #index        = np.log10(index)
+        #index_err    = 1/np.log(10) * err_index/index
 
         sscd         = np.log10(sscd)
         sfrd         = np.log10(sfrd)
@@ -420,8 +420,6 @@ class ToolsNGC3110():
         ax.set_xlabel(r"log Spectral Index")
         ax.set_ylabel(r"log SFE (yr$^{-1}$)")
         ax.set_aspect('equal', adjustable='box')
-
-        print(index)
 
         cax = ax.scatter(index, sfe_fix, s=100, c=dist_kpc, cmap="rainbow_r", linewidths=0, alpha=0.7,zorder=1e9)
         for i in range(len(dh2_fix)):
