c28266c036511f82041c0ca78cf4a017ff46f5e0
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-10-14 18:06:36 +0900

    update scripts_n3110_co.py

--- mycasa_plots.py
+++ mycasa_plots.py
@@ -849,8 +849,8 @@ def myax_fig2png_ann(ax,number,ra_cnt,dec_cnt,add_text=True,txtfiles=None):
 
     if number=="n3110_co_moms":
         # highlight speak
-        this_e = patches.Arc(xy=(0,-14), width=20, height=30, angle=0,
-            theta1=-45, theta2=45,
+        this_e = patches.Arc(xy=(3,-14), width=20, height=30, angle=0,
+            theta1=-60, theta2=40,
             fill=False, edgecolor="black", alpha=1.0, lw=3.0)
 
         ax.add_patch(this_e)
