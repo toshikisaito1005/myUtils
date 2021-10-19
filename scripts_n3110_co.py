586b1210a931778791518bef5e167350c78a08e2
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-10-19 18:58:32 +0900

    update scripts_n3110_co.py

--- scripts_n3110_co.py
+++ scripts_n3110_co.py
@@ -2351,15 +2351,15 @@ class ToolsNGC3110():
             6: 0.14518,
             } # Debye^2
 
-        y_hj = 3 * k_B * flux_hj / (8 * np.pi * Snu2[hj_upp] * 110.20135 * hj_upp) * 1e32 # cm^2
-        b = np.log(y_hj) + Eu[hj_upp] / Trot
+        #y_hj = 3 * k_B * flux_hj / (8 * np.pi * Snu2[hj_upp] * 110.20135 * hj_upp) * 1e32 # cm^2
+        #b = np.log(y_hj) + Eu[hj_upp] / Trot
         Qrot = self._partition_func(Trot, datacol=1, txtdata=txtdata)
         exp_rot = np.exp(h_p * 110.20135e+9 * hj_upp / k_B / Trot) - 1.
         exp_bg = np.exp(h_p * 110.20135e+9 * hj_upp / k_B / Tbg) - 1.
 
         #log_Ntot = (b + Qrot - np.log(1 - (exp_rot / exp_bg))) / np.log(10)
         #Ntot = y_hj * Qrot / (1 - (exp_rot / exp_bg)) / np.exp(Eu[hj_upp]/Trot)
-        factor = (8 * np.pi * Snu2[hj_upp] * 110.20135 * hj_upp) / (3 * k_B * Qrot * 1e32)
+        factor = (8 * np.pi**3 * Snu2[hj_upp] * 110.20135 * hj_upp) / (3 * k_B * Qrot * 1e32)
         factor = factor * (1 - (exp_rot / exp_bg)) * np.exp(-1 * Eu[hj_upp]/Trot)
         Ntot = flux_hj / factor
         log_Ntot = np.log10(Ntot)
