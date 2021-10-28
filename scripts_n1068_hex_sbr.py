f77f20163592c5a2b8806eaaaf92fbe82d6482df
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-10-28 17:49:45 +0900

    scripts_n1068_outflow.py

---
+++ scripts_n1068_hex_sbr.py
@@ -0,0 +1,137 @@
+"""
+Python class for the NGC 1068 outer region spectral scan project.
+using CASA.
+
+requirements:
+CASA Version 5.4.0-70, ananlysisUtils, astropy
+
+data:
+ALMA Band 3 Nakajima et al.
+SFR         Tsai et al. 2012
+
+usage:
+> import os
+> from scripts_n3110_co import ToolsSBR as tools
+> 
+> os.system("rm -rf *.last")
+
+paper drafts:
+Date         Filename                          To
+
+history:
+Date         Action
+2021-10-28   created
+Toshiki Saito@Nichidai/NAOJ
+"""
+
+import os, sys, glob
+import numpy as np
+
+import mycasa_tasks as mytask
+reload(mytask)
+
+from mycasa_stacking import cube_stacking
+from mycasa_sampling import *
+from mycasa_plots import *
+from mycasa_pca import *
+
+###########################
+### ToolsDense
+###########################
+class ToolsSBR():
+    """
+    Class for the NGC 1068 outer region spectral scan project.
+    """
+
+    ############
+    # __init__ #
+    ############
+
+    def __init__(
+        self,
+        keyfile_fig = None,
+        keyfile_gal = None,
+        refresh = False,
+        delete_inter = True,
+        ):
+        # initialize keys
+        self.keyfile_gal = keyfile_gal
+        self.keyfile_fig = keyfile_fig
+
+        # intialize task
+        self.refresh      = refresh
+        self.delete_inter = delete_inter
+        self.taskname     = None
+
+        # initialize directories
+        self.dir_raw      = None
+        self.dir_ready    = None
+        self.dir_other    = None
+        self.dir_products = None
+
+        # import parameters
+        if keyfile_fig is not None:
+            self.modname = "ToolsDense."
+            
+            # get directories
+            self.dir_proj     = self._read_key("dir_proj")
+            self.dir_raw      = self.dir_proj + self._read_key("dir_raw")
+            self.dir_ready    = self.dir_proj + self._read_key("dir_ready")
+            self.dir_other    = self.dir_proj + self._read_key("dir_other")
+            self.dir_products = self.dir_proj + self._read_key("dir_products")
+            self.dir_final    = self.dir_proj + self._read_key("dir_final")
+
+            self._create_dir(self.dir_ready)
+            self._create_dir(self.dir_products)
+            self._create_dir(self.dir_final)
+
+    ###################
+    # run_ngc1068_sbr #
+    ###################
+
+    def run_ngc1068_sbr(
+        self,
+        do_prepare = False,
+        ):
+        """
+        This method runs all the methods which will create figures in the paper.
+        """
+
+        if do_prepare==True:
+            self.align_maps()
+
+    ###############
+    # _create_dir #
+    ###############
+
+    def _create_dir(self, this_dir):
+
+        if self.refresh==True:
+            print("## refresh " + this_dir)
+            os.system("rm -rf " + this_dir)
+
+        if not glob.glob(this_dir):
+            print("## create " + this_dir)
+            os.mkdir(this_dir)
+
+        else:
+            print("## not refresh " + this_dir)
+
+    #############
+    # _read_key #
+    #############
+
+    def _read_key(self, key, keyfile="fig", delimiter=",,,"):
+
+        if keyfile=="gal":
+            keyfile = self.keyfile_gal
+        elif keyfile=="fig":
+            keyfile = self.keyfile_fig
+
+        keydata  = np.loadtxt(keyfile,dtype="str",delimiter=delimiter)
+        keywords =\
+             np.array([s.replace(" ","") for s in keydata[:,0]])
+        values   = keydata[:,1]
+        value    = values[np.where(keywords==key)[0][0]]
+
+        return value
