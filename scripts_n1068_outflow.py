30f936e37d2baf83f53e0fdb23d311e0fb9a67b5
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-10-29 16:27:47 +0900

    scripts_n1068_outflow.py

--- scripts_n1068_outflow.py
+++ scripts_n1068_outflow.py
@@ -391,6 +391,7 @@ class ToolsOutflow():
         print("################################")
 
         files   = glob.glob(self.png_outflow_model.replace("thismodel","best").replace("thisvel","*"))
+        files.sort()
         outfile = self.final_chan_model_best
         #
         png_list_ready = []
