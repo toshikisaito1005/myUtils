78441096d37a378f026531611d28ccb23c4f902e
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-02 16:32:47 +0900

    update hex_sbr

--- scripts_n1068_hex_sbr.py
+++ scripts_n1068_hex_sbr.py
@@ -218,9 +218,9 @@ class ToolsSBR():
 
         # text
         if xlim!=None and ylim!=None:
-            ax1.plot(xlim, ylim, "-", colro="black")
-            ax1.plot(xlim, [ylim[0]-1.0,ylim[1]-1.0], "-", colro="black")
-            ax1.plot(xlim, [ylim[0]+1.0,ylim[1]+1.0], "-", colro="black")
+            ax1.plot(xlim, ylim, "-", color="black")
+            ax1.plot(xlim, [ylim[0]-1.0,ylim[1]-1.0], "-", color="black")
+            ax1.plot(xlim, [ylim[0]+1.0,ylim[1]+1.0], "-", color="black")
 
         # save
         plt.savefig(output, dpi=self.fig_dpi)
