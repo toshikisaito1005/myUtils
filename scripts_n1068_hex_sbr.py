1a77402defc97a147306a97fa531a9f4cb29e60f
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-02 14:21:06 +0900

    update hex_sbr

--- scripts_n1068_hex_sbr.py
+++ scripts_n1068_hex_sbr.py
@@ -164,7 +164,7 @@ class ToolsSBR():
         name_emom0 = np.array(header[len_data+2:])
 
         n2hp_mom0  = data_mom0[:,np.where(name_mom0=="n2hp10")]
-        print(np.where(name_mom0=="n2hp10"))
+        print(n2hp_mom0)
 
         # constrain data
         cut = np.where(dist_kpc>=self.r_sbr)
