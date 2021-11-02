71bee5870fa39a87523ea5875c3303305fe9e137
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-02 14:20:37 +0900

    update hex_sbr

--- scripts_n1068_hex_sbr.py
+++ scripts_n1068_hex_sbr.py
@@ -160,8 +160,8 @@ class ToolsSBR():
 
         data_mom0  = data[:,2:len_data+2]
         data_emom0 = data[:,len_data+2:]
-        name_mom0  = header[2:len_data+2]
-        name_emom0 = header[len_data+2:]
+        name_mom0  = np.array(header[2:len_data+2])
+        name_emom0 = np.array(header[len_data+2:])
 
         n2hp_mom0  = data_mom0[:,np.where(name_mom0=="n2hp10")]
         print(np.where(name_mom0=="n2hp10"))
