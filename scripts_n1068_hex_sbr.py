525f0e861762ecb973a79620753ea1ce4249bb21
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-03 18:00:23 +0900

    update hex_sbr

--- scripts_n1068_hex_sbr.py
+++ scripts_n1068_hex_sbr.py
@@ -236,7 +236,7 @@ class ToolsSBR():
         ax.set_aspect('equal', adjustable='box')
 
         # plot
-        im = ax.scatter(x, y, s=1000, c=c, cmap="gnuplot", marker="h", linewidths=0)
+        im = ax.scatter(x, y, s=1000, c=c, cmap="rainbow", marker="h", linewidths=0)
 
         # cbar
         cbar = plt.colorbar(im)
