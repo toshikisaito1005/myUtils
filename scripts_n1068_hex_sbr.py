3faca2b0470067644186ad2ca1a9f9d112125b26
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-03 23:21:42 +0900

    update hex_sbr

--- scripts_n1068_hex_sbr.py
+++ scripts_n1068_hex_sbr.py
@@ -191,7 +191,8 @@ class ToolsSBR():
         data_mom0 = data[:,2:len_data+2]
 
         data_n2hp = data_mom0[np.where(header=="n2hp10")]
-        print(data_n2hp)
+        print(header)
+        print(np.where(header=="n2hp10"))
 
         # plot
         this_x    = ra[data_n2hp>0]
