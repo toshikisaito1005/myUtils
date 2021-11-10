6716459cfe6bad82208cbf45e53a748931092952
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-10 19:48:55 +0900

    update hex_center

--- scripts_n1068_hex_center.py
+++ scripts_n1068_hex_center.py
@@ -263,13 +263,13 @@ class ToolsPCA():
             this_r13co = list_r13co[:,i]
             this_rhcn  = list_rhcn[:,i]
 
-            sum_mom0   = np.mean(this_mom0[np.where(this_mom0!=0)])
-            sum_r13co  = np.mean(this_r13co[np.where(this_r13co!=0)])
-            sum_rhcn   = np.mean(this_rhcn[np.where(this_rhcn!=0)])
+            sum_mom0   = np.mean(this_mom0)#[np.where(this_mom0!=0)])
+            sum_r13co  = np.mean(this_r13co)#[np.where(this_r13co!=0)])
+            sum_rhcn   = np.mean(this_rhcn)#[np.where(this_rhcn!=0)])
 
-            list_mom0_mean  = np.c_[list_mom0_mean, np.where(this_mom0!=0, this_mom0/sum_mom0, 0)]
-            list_r13co_mean = np.c_[list_r13co_mean, np.where(this_r13co!=0, this_r13co/sum_r13co, 0)]
-            list_rhcn_mean  = np.c_[list_rhcn_mean, np.where(this_rhcn!=0, this_rhcn/sum_rhcn, 0)]
+            list_mom0_mean  = np.c_[list_mom0_mean, this_mom0/sum_mom0]#np.where(this_mom0!=0, this_mom0/sum_mom0, 0)]
+            list_r13co_mean = np.c_[list_r13co_mean, this_r13co/sum_r13co]#np.where(this_r13co!=0, this_r13co/sum_r13co, 0)]
+            list_rhcn_mean  = np.c_[list_rhcn_mean, this_rhcn/sum_rhcn:#np.where(this_rhcn!=0, this_rhcn/sum_rhcn, 0)]
 
         list_mom0_mean  = list_mom0_mean[:,1:].T
         list_r13co_mean = list_r13co_mean[:,1:].T
