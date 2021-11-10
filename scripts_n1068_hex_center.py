fcab38f3d3a9a4db3fcda558563e379fad182cc5
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-10 18:08:57 +0900

    update hex_center

--- scripts_n1068_hex_center.py
+++ scripts_n1068_hex_center.py
@@ -152,6 +152,7 @@ class ToolsPCA():
         data_emom0 = data[:,len_data+2:]
         name_mom0  = np.array(header[2:len_data+2])
         name_emom0 = np.array(header[len_data+2:])
+        print(name_mom0)
 
         mom0_13co  = data_mom0[:,np.where(name_mom0=="13co10")[0][0]]
         emom0_13co = data_emom0[:,np.where(name_mom0=="13co10")[0][0]]
