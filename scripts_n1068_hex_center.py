eefd9bcbc587b4378b105eadb55e14fb4d3473f8
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-11 17:16:36 +0900

    update hex_center

--- scripts_n1068_hex_center.py
+++ scripts_n1068_hex_center.py
@@ -279,8 +279,8 @@ class ToolsPCA():
             this_flux[np.isinf(this_flux)] = 0
 
             # masking
-            this_flux = np.where(r<=self.r_sbr, this_flux, 0)
-            this_err  = np.where(r<=self.r_sbr, this_err, 0)
+            this_flux = np.where(r<=self.r_sbr_as, this_flux, 0)
+            this_err  = np.where(r<=self.r_sbr_as, this_err, 0)
 
             # limiting by #data
             len_data = len(this_flux[this_flux>0])
