1f45ee17bab28949f398b20cdfb681e940c6ea51
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-19 16:25:59 +0900

    update hex_center

--- scripts_n1068_hex_center.py
+++ scripts_n1068_hex_center.py
@@ -1484,8 +1484,11 @@ class ToolsPCA():
 
         if ann==1:
             ax.text(0.97, 0.93, "CN(1$_{3/2}$-0$_{1/2}$)", color=cm.rainbow(0/float(len(clist)-1)), transform=ax.transAxes, weight="bold", fontsize=24, ha="right")
-            ax.text(0.97, 0.93, "HNC(1-0)", color=cm.rainbow(1/float(len(clist)-1)), transform=ax.transAxes, weight="bold", fontsize=24, ha="right")
-            ax.text(0.97, 0.93, "HCN(1-0)", color=cm.rainbow(1/float(len(clist)-1)), transform=ax.transAxes, weight="bold", fontsize=24, ha="right")
+            ax.text(0.97, 0.83, "HNC(1-0)", color=cm.rainbow(1/float(len(clist)-1)), transform=ax.transAxes, weight="bold", fontsize=24, ha="right")
+            ax.text(0.97, 0.73, "HCN(1-0)", color=cm.rainbow(1/float(len(clist)-1)), transform=ax.transAxes, weight="bold", fontsize=24, ha="right")
+        elif ann==2:
+            ax.text(0.97, 0.93, "CN(1$_{3/2}$-0$_{1/2}$)/HCN(1-0)", color=cm.rainbow(0/float(len(clist)-1)), transform=ax.transAxes, weight="bold", fontsize=24, ha="right")
+            ax.text(0.97, 0.83, "HNC(1-0)/HCN(1-0)", color=cm.rainbow(1/float(len(clist)-1)), transform=ax.transAxes, weight="bold", fontsize=24, ha="right")
 
         # save
         os.system("rm -rf " + outpng)
