3b51d4e92e4f26b8e1946748d6eddf1d90b339c5
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-23 17:32:13 +0900

    update hex_center

--- scripts_n1068_hex_center.py
+++ scripts_n1068_hex_center.py
@@ -497,7 +497,6 @@ class ToolsPCA():
             data_line = np.log10(line_zn + line_zs + line_zc)
         elif cone=="out":
             data_line = np.log10(data_line - line_zn - line_zs)
-            print(data_line)
 
         return data_line, r
 
