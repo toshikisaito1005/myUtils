fa66790dca89c9dd3ff12f1de60128723d8683ab
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-23 18:38:21 +0900

    update hex_center

--- scripts_n1068_hex_center.py
+++ scripts_n1068_hex_center.py
@@ -453,7 +453,7 @@ class ToolsPCA():
             size=1000/10,
             ylabel="log Ratio",
             xlim=[0,10.2],
-            ylim=[-1.6,1.1],
+            ylim=[-1.4,0.8],#[-1.6,1.1],
             ann=2,
             )
 
@@ -467,7 +467,7 @@ class ToolsPCA():
             size=1000/10,
             ylabel="log Ratio",
             xlim=[0,10.2],
-            ylim=[-1.6,1.1],
+            ylim=[-1.4,0.8],#[-1.6,1.1],
             )
 
     ######################
