596c6e1f6a1e8615f6231f268bf12bf6678ac598
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-24 18:05:03 +0900

    update hex_center

--- scripts_n1068_outflow.py
+++ scripts_n1068_outflow.py
@@ -1231,7 +1231,7 @@ class ToolsOutflow():
             "log [CI]/CO",
             "log [SIII]/[SII]",
             "(e) log [SIII]/[SII] vs. log [CI]/CO",
-            [-3.0,3.0], [-0.6,1.0],
+            [-3.0,3.0], [-1.0,1.5],
             plot_line = False,
             )
 
