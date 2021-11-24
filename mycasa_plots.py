1c6bca8a36baec959e3322fc6cc47128670d3e98
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-11-24 16:49:19 +0900

    update hex_center

--- mycasa_plots.py
+++ mycasa_plots.py
@@ -838,6 +838,7 @@ def myax_fig2png_ann(ax,number,ra_cnt,dec_cnt,add_text=True,txtfiles=None):
                 horizontalalignment="right", verticalalignment="center", weight="bold")
 
             ax.plot([7,7.5], [2.5,7.0], "--", lw=3, c="black")
+            ax.plot([-1,4.5], [6.0,7.5], "--", lw=3, c="black")
             ax.text(8.5, 8.0, "Bicone",
                 horizontalalignment="left", verticalalignment="center", weight="bold")
             ax.text(8.5, 7.3, "boundary",
