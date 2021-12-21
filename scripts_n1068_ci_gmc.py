94b894052b3657a428466c762446a6dd035a6bf3
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-12-21 16:26:13 +0900

    update ci_gmc

--- scripts_n1068_ci_gmc.py
+++ scripts_n1068_ci_gmc.py
@@ -240,16 +240,19 @@ class ToolsCIGMC():
         d, h = hdu.data, hdu.header
         h["CTYPE3"] = "VELOCITY"
         fits.PrimaryHDU(d, h).writeto(self.hcn10_ready, overwrite=True)
+        os.system("rm -rf "self.hcn10_ready + "2")
 
-        hdu = fits.open(self.cube_co10+"2")[0]
+        hdu = fits.open(self.co10_ready+"2")[0]
         d, h = hdu.data, hdu.header
         h["CTYPE3"] = "VELOCITY"
-        fits.PrimaryHDU(d, h).writeto(self.cube_co10, overwrite=True)
+        fits.PrimaryHDU(d, h).writeto(self.co10_ready, overwrite=True)
+        os.system("rm -rf "self.co10_ready + "2")
 
-        hdu = fits.open(self.cube_ci10+"2")[0]
+        hdu = fits.open(self.ci10_ready+"2")[0]
         d, h = hdu.data, hdu.header
         h["CTYPE3"] = "VELOCITY"
-        fits.PrimaryHDU(d, h).writeto(self.cube_ci10, overwrite=True)
+        fits.PrimaryHDU(d, h).writeto(self.ci10_ready, overwrite=True)
+        os.system("rm -rf "self.ci10_ready + "2")
 
     ###############
     # _create_dir #
