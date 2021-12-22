e30d72cad0d23b1827dfe7cc71e4bd59bff870a8
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-12-22 19:18:31 +0900

    update hex_sbr

--- scripts_n1068_hex_sbr.py
+++ scripts_n1068_hex_sbr.py
@@ -573,7 +573,7 @@ class ToolsSBR():
         data_c18o = data_mom0[:,np.where(header=="c18o10")[0][0]]
 
         # masking (1) center
-        mask = np.where(dist_kpc<self.r_sbr,2,0)
+        mask = np.where((dist_kpc<self.r_sbr)&(theta_deg>=-15)&(theta_deg<65),2,0)
         data_c18o_masked = np.where(dist_kpc<self.r_sbr,0,data_c18o)
 
         # masking (2) CND
