b792accfeb5bc8e3af664e9772c42d588922a4c2
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-12-22 12:54:11 +0900

    update ci_gmc

--- scripts_n1068_ci_gmc.py
+++ scripts_n1068_ci_gmc.py
@@ -345,7 +345,7 @@ class ToolsCIGMC():
             cut         = np.where((this_tb["S2N"]>=snr) & (this_tb["RAD_PC"]>=radlimit) & (this_tb["RAD_PC"]<=radlimit*5))
             this_x      = (this_tb["XCTR_DEG"][cut] - self.ra_agn) * 3600.  
             this_y      = (this_tb["YCTR_DEG"][cut] - self.dec_agn) * 3600.  
-            this_c      = this_tb[this_param][this_tb["S2N"]>=snr]
+            this_c      = this_tb[this_param][cut]
             this_outpng = outpng_header+"_" + this_footer + ".png"
 
             self._plot_cpropsmap(
