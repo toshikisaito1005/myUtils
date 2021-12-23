39c0e38791465767d667d0967412fada74e17473
toshikisaito1005 <toshiki.saito1005@gmail.com>
2021-12-23 18:41:54 +0900

    update hex_sbr

--- scripts_n1068_hex_sbr.py
+++ scripts_n1068_hex_sbr.py
@@ -495,6 +495,8 @@ class ToolsSBR():
         theta_deg = np.degrees(np.arctan2(ra, dec))
         data_mom0 = data
 
+        print(header)
+        print(np.shape(header))
         print(np.shape(data_mom0))
 
     ################
