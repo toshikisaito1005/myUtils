4feb7a250d4f3c0ecbf3376156d0026a5e9c436c
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-01-21 16:26:10 +0900

    update hex_pca

--- scripts_n1068_hex_pca.py
+++ scripts_n1068_hex_pca.py
@@ -1618,7 +1618,7 @@ class ToolsPCA():
                     ann      = True,
                     add_text = False,
                     lim      = 30,
-                    size     = 500,
+                    size     = 600,
                     label    = "(K km s$^{-1}$)",
                     )  
 
