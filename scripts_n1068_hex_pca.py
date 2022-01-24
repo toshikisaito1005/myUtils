9eab9f0f81cf9a05ceabcf06104d081913f46ef4
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-01-24 14:57:36 +0900

    update hex_pca

--- scripts_n1068_hex_pca.py
+++ scripts_n1068_hex_pca.py
@@ -770,7 +770,10 @@ class ToolsPCA():
 
                 med_cnd   = np.round(np.max(this_cnd[this_cnd>0]), 2)
                 med_out   = np.round(np.max(this_out[this_out>0]), 2)
-                med_disk  = np.round(np.max(this_disk[this_disk>0]), 2)
+                if this_disk[this_disk>0]>0:
+                    med_disk = np.round(np.max(this_disk[this_disk>0]), 2)
+                else:
+                    med_disk = 0
 
                 print(this_name, med_cnd, med_out, med_disk)
 
