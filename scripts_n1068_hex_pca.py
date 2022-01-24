c4762b953197699caf77c0d69422680c0958ed8e
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-01-24 15:46:23 +0900

    update hex_pca

--- scripts_n1068_hex_pca.py
+++ scripts_n1068_hex_pca.py
@@ -785,7 +785,7 @@ class ToolsPCA():
         list_output = np.c_[list_name, list_cnd, list_out, list_disk]
 
         list_output_sort = list_output[ np.argsort(list_output[:,2].astype("float32")/list_output[:,3].astype("float32")) ]
-        list_output_sort = [s for s in list_output_sort if not "siiisii_ratio" in s]
+        list_output_sort = [s for s in list_output_sort if not "siiisii_ratio" in s[0]]
         siiisii_ratio    = list_output_sort[list_output_sort[:,0]=="siiisii_ratio"]
         print(list_output_sort)
         print(siiisii_ratio)
