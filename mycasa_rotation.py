0f14d369d8e79f0be0c647f49d95ef62dac38795
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-02-02 20:21:04 +0900

    update n1068_ncol

--- mycasa_rotation.py
+++ mycasa_rotation.py
@@ -160,7 +160,8 @@ def rotation_13co21_13co10(
             e1 = perr[1]
             e3 = abs(perr[3])
 
-            map_mom0_low[this_x,this_y]   = p0 * p3 * np.sqrt(2*np.pi)
+            if p0>0 and p1>0 and p2!=guess_b and p3!=40:
+                map_mom0_low[this_x,this_y]   = p0 * p3 * np.sqrt(2*np.pi)
 
             """
             #if pr>0 and pr<=ratio_max and p2!=guess_b and p3!=40 and p0<max_low and p0>0 and p1<max_high and p1>0:
