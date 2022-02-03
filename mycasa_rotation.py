e24851af454a04fefdd4a2f1c2be0dc0ca85e120
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-02-03 12:23:50 +0900

    update n1068_ncol

--- mycasa_rotation.py
+++ mycasa_rotation.py
@@ -205,7 +205,7 @@ def rotation_13co21_13co10(
                 x_data       = np.array([Eu_low, Eu_high])
                 y_data       = np.array([log10_Nugu_low, log10_Nugu_high])
                 y_err        = np.array([elog10_Nugu_low, elog10_Nugu_high])
-                popt2, pcov2 = curve_fit(_f_linear,x_data,y_data,sigma=y_err,p0=p0_rotation,maxfev=100000)
+                popt2, pcov2 = curve_fit(_f_linear,x_data,y_data,sigma=y_err,p0=p0_rotation,maxfev=100000,absolute_sigma=True)
                 perr2        = np.sqrt(np.diag(pcov2))
 
                 Trot  = np.log10(np.e) / popt2[0]
