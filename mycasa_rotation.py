adf0d91c6761bc0c13c485151ac5f8f724e263bb
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-02-03 13:42:28 +0900

    update n1068_ncol

--- mycasa_rotation.py
+++ mycasa_rotation.py
@@ -253,6 +253,7 @@ def rotation_13co21_13co10(
             e0 = perr[0]
             e2 = perr[1]
             e3 = abs(perr[2])
+            print(p0>0,p0<max_low,pr>0,pr<=ratio_max,p2!=guess_b,p3!=40,p0/e0>snr)
 
             if p0>0 and p0<max_low and pr>0 and pr<=ratio_max and p2!=guess_b and p3!=40 and p0/e0>snr:
                 print("# fit only 1-0")
