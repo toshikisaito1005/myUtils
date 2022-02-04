d1c2c66ec12923e0abe663f8d23ee6f13550f7a9
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-02-04 11:56:04 +0900

    update n1068_ncol

--- scripts_n1068_ncol_13co.py
+++ scripts_n1068_ncol_13co.py
@@ -1432,7 +1432,7 @@ class ToolsNcol():
         snr   = 5.0
         scale = immax / snr
         pix   = abs(imhead("../data_ready/n1068_13co21_60pc.cube.fits",mode="list")["cdelt1"]) * 3600 * 180 / np.pi
-        beam  = abs(imhead(self.outmodelcube_13co10,mode="list")["cdelt1"])["beammajor"]["value"]
+        beam  = imhead(self.outmodelcube_13co10,mode="list")["cdelt1"]["beammajor"]["value"]
 
         # create noise
         os.system("rm -rf noise.fits")
