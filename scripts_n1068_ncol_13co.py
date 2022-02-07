cd81769ef8e435a943e2964d22c7d60fc84fdf74
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-02-07 17:46:17 +0900

    update n1068_ncol

--- scripts_n1068_ncol_13co.py
+++ scripts_n1068_ncol_13co.py
@@ -1636,13 +1636,13 @@ class ToolsNcol():
         fig = plt.figure(figsize=(13,10))
         gs  = gridspec.GridSpec(nrows=10, ncols=10)
         ax1 = plt.subplot(gs[0:10,0:10])
-        ad  = [0.19,0.99,0.10,0.90]
+        ad  = [0.215,0.83,0.10,0.90]
         myax_set(ax1, "both", lim, lim, None, "13co10", "13co21", adjust=ad)
 
         sc = ax1.scatter(x, y, c=r, cmap="rainbow_r", lw=0, s=20, zorder=1e9)
         plt.errorbar(x, y, xerr, yerr, lw=1, capsize=0, color="grey", linestyle="None")
 
-        """
+        """ cmap for errorbar
         clb   = plt.colorbar(sc)
         color = clb.to_rgba(r)
         for this_x, this_y, this_xerr, this_yerr, this_c in zip(x, y, xerr, yerr, color):
