a2646285499fa617d91c13008e542dfaff825e8a
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-02-08 15:41:17 +0900

    update n1068_ncol

--- scripts_n1068_ncol_13co.py
+++ scripts_n1068_ncol_13co.py
@@ -1742,7 +1742,7 @@ class ToolsNcol():
         ax1.plot(n-T_all, t_grid, lw=2, color="black")
         ax1.fill_between([n-T_all,n+T_all], t_grid, color="grey")
 
-        n = 2
+        n = 3
         ax1.plot(n+T_cnd, t_grid, lw=2, color="black")
         ax1.plot(n-T_cnd, t_grid, lw=2, color="black")
         ax1.fill_between([n-T_cnd,n+T_cnd], t_grid, color="tomato")
