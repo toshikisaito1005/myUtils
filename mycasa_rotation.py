e9d01a14057a6e053e3565e01b168c7c28025f9d
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-02-21 15:14:40 +0900

    update n1068_ncol

--- mycasa_rotation.py
+++ mycasa_rotation.py
@@ -164,17 +164,17 @@ def rotation_13co21_13co10(
             perr = np.sqrt(np.diag(pcov))
 
             # Q-Q plot
-            qf_two     = np.array( _f_one( this_freq, popt[0], popt[2], abs(popt[3]), restfreq_low ) )
+            qf_one = np.array( _f_one( this_freq, popt[0], popt[2], abs(popt[3]), restfreq_low ) )
             print(qf_two)
             qthres     = popt[0] * 0.026673718259424 # 1st or 99th percentiles
             qthres2    = popt[0] * 0.24197072451914  # +/-1sigma
-            if len(np.where(qf_two>qthres)[0])>3:
-                qrange  = [ np.where(qf_two>qthres)[0][0], np.where(qf_two>qthres)[0][-1]+1 ]
-                qrange2 = [ np.where(qf_two>qthres2)[0][0], np.where(qf_two>qthres2)[0][-1]+1 ]
+            if len(np.where(qf_one>qthres)[0])>3:
+                qrange  = [ np.where(qf_one>qthres)[0][0], np.where(qf_one>qthres)[0][-1]+1 ]
+                qrange2 = [ np.where(qf_one>qthres2)[0][0], np.where(qf_one>qthres2)[0][-1]+1 ]
 
                 #
                 qdata  = this_data[qrange[0]:qrange[1]]
-                qmodel = qf_two[qrange[0]:qrange[1]]
+                qmodel = qf_one[qrange[0]:qrange[1]]
                 qmean  = np.argmax(qmodel)
                 qstd   = (qrange2[1] - qrange2[0]) / 2.0
 
