860522de806e8247477a33c97056839df18fd467
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-09 19:26:26 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -336,8 +336,8 @@ class ToolsLSTSim():
             X   = list_dist * np.cos(d)
             Y   = list_dist * np.sin(d)
 
-            this_u = X*np.sin(H_h) #+ Y*np.cos(H_h)
-            this_v = Y*np.cos(dec) - X*np.sin(dec)*np.cos(H_h)
+            this_u =  X*np.sin(H_h)             - Y*np.cos(H_h)
+            this_v = -X*np.sin(dec)*np.cos(H_h) + Y*np.sin(dec)*np.sin(H_h)
             list_u = np.r_[list_u, this_u]
             list_v = np.r_[list_v, this_v]
 
