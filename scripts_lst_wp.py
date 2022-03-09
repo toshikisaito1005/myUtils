84ac9dcd7f1f41c932921f3f94a21a1cb095188c
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-09 22:40:23 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -229,10 +229,10 @@ class ToolsLSTSim():
         taskname = self.modname + sys._getframe().f_code.co_name
         check_first(self.config_c10,taskname)
 
-        decl = -37.755 # 0=celestial equator, 90=north pole, -90=south pole
+        decl = 37.755 # 0=celestial equator, 90=north pole, -90=south pole
         tinteg = 2
-        lst_position = np.array([0,0,0]) # km/s
-        #lst_position = np.array([6.452141+0.1, 7.886675+0.1, -0.245131]) # km/s
+        #lst_position = np.array([0,0,0]) # km/s
+        lst_position = np.array([6.452141+0.1, 7.886675+0.1, -0.245131]) # km/s
 
         # get data
         data  = np.loadtxt(self.config_c10,"str")
