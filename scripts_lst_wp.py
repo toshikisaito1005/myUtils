0e979cf68b9d24631b2965f6854f6dc3605086cd
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-09 20:40:00 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -347,8 +347,8 @@ class ToolsLSTSim():
         for this_t in trange:
             H = np.radians(this_t)
 
-            this_u = X*np.sin(H) + Y*np.cos(H)
-            this_v = -X*np.sin(D)*np.cos(H) + Y*np.sin(D)*np.sin(H) + Z*np.cos(D)
+            this_u = X[1]*np.sin(H) + Y[1]*np.cos(H)
+            this_v = -X[1]*np.sin(D)*np.cos(H) + Y[1]*np.sin(D)*np.sin(H) + Z[1]*np.cos(D)
 
             # output
             list_u = np.r_[list_u, this_u]
