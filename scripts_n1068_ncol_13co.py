794ed34faf341bc3488b103ebdaa939581afa18f
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-10 20:35:38 +0900

    update ncol_13co

--- scripts_n1068_ncol_13co.py
+++ scripts_n1068_ncol_13co.py
@@ -1841,7 +1841,7 @@ class ToolsNcol():
         sfr = 4.6e-28 * (Te/1.e4)**-0.45 * (nu/1.e9)**0.1 / ( 2 * np.pi * beam_sigma**2 / pixesize**2 ) * ( 1.2e27 * self.distance**2 * (1+self.redshift)**-3 ) * 1000.
 
         # calc
-        run_immath_one(
+        run_immath_two(
             self.outmaps_band3,
             self.outmaps_band8_fov1,
             self.outmaps_sfr + "_tmp1",
