a46c6b0e1e2c97bf045a11f8cd2146fb71b67521
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-11 16:07:21 +0900

    update ncol_13co

--- scripts_n1068_ncol_13co.py
+++ scripts_n1068_ncol_13co.py
@@ -1834,13 +1834,12 @@ class ToolsNcol():
         beam_fwhm = imhead(self.outmaps_band3, mode="list")["beammajor"]["value"]
         pixesize = abs(imhead(self.outmaps_band3)["incr"][0])*180/np.pi*3600
         beam_sigma = beam_fwhm / (2*np.sqrt(2*np.log(2)))
-        print(2 * np.pi * beam_sigma**2 / pixesize**2)
 
         # calc sfr
         JyB_to_Jy = 1 / ( 2 * np.pi * beam_sigma**2 / pixesize**2 )
         JyB_to_L = JyB_to_Jy * 1.2e27 * self.distance**2 * (1+self.redshift)**-3
         sfr = 4.6e-28 * (Te/1.e4)**-0.45 * (nu/1.e9)**0.1 * JyB_to_L
-        sfr_density = sfr / (pixesize**2 * self.scale_pc**2)
+        sfr_density = sfr #/ (pixesize * self.scale_pc)**2
 
         # mask center
         run_importfits(self.outmaps_band8_fov1,self.outmaps_band8_fov1+"_tmp1",defaultaxes=True,defaultaxesvalues=["RA","Dec","1GHz","Stokes"])
