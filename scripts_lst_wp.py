a2f4384f1c1e609342e97d07bc2abd08e8347086
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-16 19:02:06 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -653,7 +653,7 @@ class ToolsLSTSim():
         ax1.add_patch(antenna)
 
         # ann
-        xpos = x_7m[np.argmax(y_7m*1000.)]
+        xpos = x_7m[np.argmax(y_7m*1000.)] * 1000.
         print(xpos)
         ax1.plot([xpos,xpos],[np.max(y_7m*1000.),-457.2313425-y_cnt],"-",color="black",lw=2)
         ax1.text(1,77,"90m $\simeq$ 50m x cot 30$^{\circ}$",ha="left",fontsize=17)
