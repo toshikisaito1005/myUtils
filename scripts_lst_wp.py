01e46fe6e4e15c84784a7f231ffe0063c0af29af
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-16 18:49:17 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -653,9 +653,9 @@ class ToolsLSTSim():
 
         # ann
         ypos = (-457.2313425-y_cnt-np.max(this_y)) / 2.0
-        ax1.plot([0,0],[np.max(y_7m),-457.2313425-y_cnt],"-",color="black")
-        ax1.text(1,ypos,"90m $\simeq$ 50m x cot 30$^{\circ}$",ha="left")
-        ax1.text(1,ypos-10,"No shadowing around El. $\simeq$ 30$^{\circ}$",ha="left")
+        ax1.plot([0,0],[np.max(y_7m*1000.),-457.2313425-y_cnt],"-",color="black")
+        ax1.text(1,ypos,"90m $\simeq$ 50m x cot 30$^{\circ}$",ha="left",fontsize=16)
+        ax1.text(1,ypos-10,"No shadowing around El. $\simeq$ 30$^{\circ}$",ha="left",fontsize=16)
 
         # text
         ax1.text(0.05,0.92, "ACA 7-m array", color="deepskyblue", weight="bold", transform=ax1.transAxes)
