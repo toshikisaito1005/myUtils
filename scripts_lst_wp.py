b619ddf5e74ad419093e582ab1ea1ef5d751f7c9
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-16 19:27:16 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -662,27 +662,22 @@ class ToolsLSTSim():
         for i in range(len(x_12m)):
             this_x = x_12m[i]
             this_y = y_12m[i]
-            antenna = patches.Ellipse(xy=(this_x,this_y), width=12.0/1000.,
-                height=12.0/1000., angle=0, fill=True, color="lightgreen", edgecolor="lightgreen",
+            antenna = patches.Ellipse(xy=(this_x,this_y), width=12.0/10.,
+                height=12.0/10., angle=0, fill=True, color="lightgreen", edgecolor="lightgreen",
                 alpha=1.0, lw=0)
             ax1.add_patch(antenna)
 
         # LST 0
-        antenna = patches.Ellipse(xy=(-50.06162725/1000.-x_cnt/1000.,-457.2313425/1000.-y_cnt/1000.), width=50.0/1000.,
-            height=50.0/1000., angle=0, fill=True, color="tomato", edgecolor="tomato",
+        antenna = patches.Ellipse(xy=(-50.06162725/1000.-x_cnt/1000.,-457.2313425/1000.-y_cnt/1000.), width=50.0/10.,
+            height=50.0/10., angle=0, fill=True, color="tomato", edgecolor="tomato",
             alpha=1.0, lw=0)
         ax1.add_patch(antenna)
 
         # LST 1
-        antenna = patches.Ellipse(xy=(6.452141-x_cnt/1000.,7.886675-y_cnt/1000.+0.09), width=50.0/1000.,
-            height=50.0/1000., angle=0, fill=True, color="tomato", edgecolor="tomato",
+        antenna = patches.Ellipse(xy=(6.452141-x_cnt/1000.,7.886675-y_cnt/1000.+0.09), width=50.0/10.,
+            height=50.0/10., angle=0, fill=True, color="tomato", edgecolor="tomato",
             alpha=1.0, lw=0)
         ax1.add_patch(antenna)
-
-        #ax1.scatter(6.452141+0.1, 7.886675+0.1, color="tomato", marker="*", lw=0, s=900)
-        #ax1.scatter(8, -8, color="tomato", marker="*", lw=0, s=900)
-        #ax1.scatter(-8, 0, color="tomato", marker="*", lw=0, s=900)
-        #ax1.scatter(0, 0, color="tomato", marker="*", lw=0, s=900)
 
         # text
         ax1.text(0.05,0.92, "ALMA 12-m array", color="grey", weight="bold", transform=ax1.transAxes)
