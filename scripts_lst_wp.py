9fff72c9397dec25fbbbcdb9af692d8ce37d898f
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-16 19:01:26 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -654,6 +654,7 @@ class ToolsLSTSim():
 
         # ann
         xpos = x_7m[np.argmax(y_7m*1000.)]
+        print(xpos)
         ax1.plot([xpos,xpos],[np.max(y_7m*1000.),-457.2313425-y_cnt],"-",color="black",lw=2)
         ax1.text(1,77,"90m $\simeq$ 50m x cot 30$^{\circ}$",ha="left",fontsize=17)
         ax1.text(1,70,"(No shadowing until El. $\simeq$ 30$^{\circ}$)",ha="left",fontsize=17)
