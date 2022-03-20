d54a460d34ca20c2ab1956dd8352ca21c3834f17
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-20 22:00:47 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -529,7 +529,7 @@ class ToolsLSTSim():
 
         os.system("rm -rf test_all.* test_onlyLST.* test_noLST.*")
         tclean(vis="test.ms",imagename="test_all",gridder="mosaic",cell="0.1arcsec",imsize=[256,256])
-        tclean(vis="test.ms",imagename="test_onlyLST",gridder="mosaic",cell="0.1arcsec",imsize=[256,256],antenna="LST")
+        tclean(vis="test.ms",imagename="test_onlyLST",gridder="mosaic",cell="0.1arcsec",imsize=[256,256],antenna="test")
         tclean(vis="test.ms",imagename="test_noLST",gridder="mosaic",cell="0.1arcsec",imsize=[256,256],antenna="!LST")
 
         """
