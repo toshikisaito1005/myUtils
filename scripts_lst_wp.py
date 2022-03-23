fec9cd035912fc32eb64be926e496c99c9db540a
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-23 18:16:16 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -191,6 +191,8 @@ class ToolsLSTSim():
         self.outpng_uv_aca        = self.dir_products + self._read_key("outpng_uv_aca")
 
         self.outpng_mom0_input    = self.dir_products + self._read_key("outpng_mom0_input")
+        self.outpng_mom0_tp       = self.dir_products + self._read_key("outpng_mom0_tp")
+        self.outpng_mom0_lst50m   = self.dir_products + self._read_key("outpng_mom0_lst50m")
 
     ####################
     # run_sim_lst_alma #
@@ -418,7 +420,9 @@ class ToolsLSTSim():
         taskname = self.modname + sys._getframe().f_code.co_name
         check_first(self.n1097_template_fullspec,taskname)
 
-        # plot input
+        ##############
+        # plot input #
+        ##############
         mom0_input = "input.mom0.fits"
         os.system("rm -rf " + mom0_input+"_tmp1")
         immoments(
@@ -447,8 +451,8 @@ class ToolsLSTSim():
             width_cont1=[1.0],
             color_cont1="black",
             # imshow
-            set_title="Input n1097sim [CI] mom0",
-            colorlog=True,
+            set_title="Input: n1097sim [CI] mom0",
+            colorlog=False,
             set_cmap="rainbow",
             set_bg_color=cm.rainbow(0),
             showbeam=False,
@@ -457,7 +461,56 @@ class ToolsLSTSim():
             label_scalebar=None,
             comment=None,
             # imshow colorbar
-            clim=None,
+            clim=[0,7],
+            label_cbar="(K km s$^{-1}$)",
+            # annotation
+            numann="lst_n1097sim",
+            textann=True,
+            )
+
+        ###########
+        # plot TP #
+        ###########
+        mom0_tp = "tp.mom0.fits"
+        thres = 0.147 * 2.5
+        os.system("rm -rf " + mom0_tp+"_tmp1")
+        immoments(
+            imagename = self.dir_ready + "outputs/" + self.n1097_sdimage_fullspec.replace(".image","_"+totaltimetint+"7m.image"),
+            includepix = [thres,100000],
+            outfile = mom0_tp+"_tmp1",
+            )
+        run_exportfits(
+            imagename = mom0_tp+"_tmp1",
+            fitsimage = mom0_tp,
+            delin = True,
+            dropdeg = True,
+            dropstokes = True,
+            )
+        myfig_fits2png(
+            # general
+            mom0_tp,
+            self.outpng_mom0_tp,
+            imcontour1=mom0_tp,
+            imsize_as=150,
+            ra_cnt="41.5763deg",
+            dec_cnt="-30.2771deg",
+            # contour 1
+            unit_cont1=None,
+            levels_cont1=[0.005,0.01,0.02,0.04,0.08,0.16,0.32,0.64,0.96],
+            width_cont1=[1.0],
+            color_cont1="black",
+            # imshow
+            set_title="TP 12m: n1097sim [CI] mom0",
+            colorlog=False,
+            set_cmap="rainbow",
+            set_bg_color=cm.rainbow(0),
+            showbeam=False,
+            color_beam="black",
+            scalebar=None,
+            label_scalebar=None,
+            comment=None,
+            # imshow colorbar
+            clim=[0,7],
             label_cbar="(K km s$^{-1}$)",
             # annotation
             numann="lst_n1097sim",
