85212006d6b379d8ed4e85d7ca34603cd091553a
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-25 19:14:24 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -1315,15 +1315,15 @@ class ToolsLSTSim():
             os.mkdir(ms_dir)
 
         # assume ngc1068 torus
-        rmaj_out     = str(10.  / 72.)+"arcsec" # arcsec, 10pc at ngc1068, Gamez-Rosas et al. 2022 Nature
-        rmin_out     = str(1.74 / 72.)+"arcsec" # arcsec, 10pc at ngc1068, Gamez-Rosas et al. 2022 Nature
+        rmaj_out     = str(10.  / 72. / 2.)+"arcsec" # arcsec, 10pc at ngc1068, Gamez-Rosas et al. 2022 Nature
+        rmin_out     = str(1.74 / 72. / 2.)+"arcsec" # arcsec, 10pc at ngc1068, Gamez-Rosas et al. 2022 Nature
         pa           = '-50.0deg' # Gamez-Rosas et al. 2022 Nature
         #totalflux    = 13.8 / 1000. * 345**3.8 / 693.9640232**3.8 * 2 # continuum flux (mJy) at 432um (693.9640232 GHz), Garcia-Burillo et al. 2017
         totalflux    = 13.8 / 1000.
 
         rmaj_in      = str(10.  / 5. / 72.)+"arcsec"
         rmin_in      = str(1.74 / 5. / 72. * 2)+"arcsec"
-        totalflux_in = -1 * totalflux / 5**2
+        totalflux_in = -1 * totalflux / (5.*10.)
 
         scale = float(totalflux) / float(totalflux-totalflux_in)
         totalflux = totalflux * scale
