c8ef9535975d38b4802dd532d73c29da3c7eea62
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-03-27 15:52:47 +0900

    update lst_wp

--- scripts_lst_wp.py
+++ scripts_lst_wp.py
@@ -37,6 +37,7 @@ Toshiki Saito@Nichidai/NAOJ
 
 import os, sys, glob
 import numpy as np
+from scipy.stats import gaussian_kde
 
 from mycasa_tasks import *
 from mycasa_plots import *
@@ -538,6 +539,12 @@ class ToolsLSTSim():
         y_lst      = np.log10(data_lst[cut])
         yerr_lst   = abs(err_lst[cut]) / data_lst[cut]
 
+        # histogram
+        tlim  = [-1.0,2.0]
+        ygrid = np.linspace(tlim[0], tlim[1], num=1000)
+        l = gaussian_kde(y_7m_tp - x_7m_tp)
+        kde_7m_tp = np.array(l(ygrid) / np.sum(l(ygrid)))
+
         ########
         # plot #
         ########
@@ -572,6 +579,43 @@ class ToolsLSTSim():
         plt.subplots_adjust(hspace=.0)
         os.system("rm -rf " + self.outpng_scatter_n1097sim)
         plt.savefig(self.outpng_scatter_n1097sim, dpi=self.fig_dpi)
+
+        """
+        ##################
+        # plot histogram #
+        ##################
+        ad   = [0.215,0.83,0.10,0.90]
+        xlim = None #[-1.2,2.2]
+        ylim = None #[-1.2,2.2]
+
+        # prepare
+        fig = plt.figure(figsize=(13,10))
+        gs  = gridspec.GridSpec(nrows=10, ncols=10)
+        ax1 = plt.subplot(gs[0:10,0:10])
+        plt.subplots_adjust(left=ad[0], right=ad[1], bottom=ad[2], top=ad[3])
+        myax_set(ax1, "both", xlim, ylim, "pixel-by-pixel scatter", "Convolved input (K km s$^{^1}$)", "Convolved output (K km s$^{^1}$)", adjust=ad)
+
+        # plot
+        ax1.scatter(x_7m_tp, y_7m_tp, c="red", lw=0, s=20, zorder=1e9)
+        ax1.errorbar(x_7m_tp, y_7m_tp, yerr=yerr_7m_tp, lw=0.5, capsize=0, color="tomato", linestyle="None")
+
+        ax1.scatter(x_lst, y_lst, c="blue", lw=0, s=20, zorder=1e9)
+        ax1.errorbar(x_lst, y_lst, yerr=yerr_lst, lw=0.5, capsize=0, color="deepskyblue", linestyle="None")
+
+        # text
+        ax1.text(0.05,0.92, "7m+TP map", color="tomato", weight="bold", transform=ax1.transAxes)
+        ax1.text(0.05,0.87, "LST map", color="deepskyblue", weight="bold", transform=ax1.transAxes)
+
+        # ann
+        ax1.plot(xlim, ylim, "--", color="black", lw=2,zorder=1e10)
+        ax1.plot(xlim, [ylim[0]+1.0,ylim[1]+1.0], "--", color="black", lw=2,zorder=1e10)
+        ax1.plot(xlim, [ylim[0]-1.0,ylim[1]-1.0], "--", color="black", lw=2,zorder=1e10)
+
+        # save
+        plt.subplots_adjust(hspace=.0)
+        os.system("rm -rf " + self.outpng_scatter_n1097sim)
+        plt.savefig(self.outpng_scatter_n1097sim, dpi=self.fig_dpi)
+        """
 
     ###############
     # create_mom0 #
