b7a1baaf96b12152ccfb6b3f6b8917e91f02e8cc
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-06-20 23:02:39 +0900

    update n1068_hex_pca

--- scripts_n1068_hex_pca.py
+++ scripts_n1068_hex_pca.py
@@ -805,10 +805,11 @@ class ToolsPCA():
 
             if len(mom0)>10:
                 median_emom0 = np.median(emom0)
-                median_snr   = np.median(snr[snr>=self.snr_mom])
-                p84_snr      = np.percentile(snr[snr>=self.snr_mom],84)
-                peak_snr     = np.max(snr[snr>=self.snr_mom])
-                print(this_line, median_emom0, median_snr, peak_snr, len(mom0))
+                p16_snr      = np.round(np.percentile(snr[snr>=self.snr_mom],16),1)
+                median_snr   = np.round(np.median(snr[snr>=self.snr_mom]),1)
+                p84_snr      = np.round(np.percentile(snr[snr>=self.snr_mom],84),1)
+                peak_snr     = np.round(np.max(snr[snr>=self.snr_mom]),1)
+                print(this_line, median_emom0, p16_snr, median_snr, p84_snr, peak_snr, len(mom0))
 
     #######################
     # plot_max_line_graph # Figure 5
