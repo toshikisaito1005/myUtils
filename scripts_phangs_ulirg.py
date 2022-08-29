b4272344f3058b4193ac1b2aa63202f8b0221887
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-08-29 20:42:23 +0900

    create phangs_ulirgs

---
+++ scripts_phangs_ulirg.py
@@ -0,0 +1,437 @@
+"""
+Python class for the PHANGS-(U)LIRG project
+
+requirements:
+CASA Version 5.4.0-70, ananlysisUtils, astropy
+
+data:
+
+
+usage:
+> import os
+> from scripts_phangs_ulirg import ToolsULIRG as tools
+>
+> # key
+> tl = tools(
+>     refresh     = False,
+>     keyfile_gal = "/home02/saitots/myUtils/keys_projects/galkey_ulirg.txt",
+>     keyfile_fig = "/home02/saitots/myUtils/keys_projects/key_phangs_ulirg.txt",
+>     )
+>
+> # main
+> tl.run_phangs_ulirg(
+>     do_all           = True,
+>     # analysis
+>     do_prepare       = True,
+>     # plot
+>     plot_showcase    = True,
+>     # supplement
+>     )
+>
+> os.system("rm -rf *.last")
+
+paper drafts:
+Date         Filename                To
+
+history:
+2017-11-01   created
+2022-07-28   constructed align_cubes
+2022-07-29   constructed multismooth
+Toshiki Saito@NAOJ
+"""
+
+import os, sys, glob
+import numpy as np
+from scipy.stats import gaussian_kde
+
+from mycasa_sampling import *
+from mycasa_tasks import *
+from mycasa_plots import *
+
+##############
+# ToolsULIRG #
+##############
+class ToolsULIRG():
+    """
+    Class for the PHANGS-(U)LIRG project.
+    """
+
+    ############
+    # __init__ #
+    ############
+
+    def __init__(
+        self,
+        keyfile_fig  = None,
+        keyfile_gal  = None,
+        refresh      = False,
+        delete_inter = True,
+        ):
+        # initialize keys
+        self.keyfile_gal = keyfile_gal
+        self.keyfile_fig = keyfile_fig
+
+        # intialize task
+        self.refresh      = refresh
+        self.delete_inter = delete_inter
+        self.taskname     = None
+
+        # initialize directories
+        self.dir_raw      = None
+        self.dir_ready    = None
+        self.dir_other    = None
+        self.dir_products = None
+        self.fig_dpi      = 200
+
+        # import parameters
+        if keyfile_fig is not None:
+            self.modname = "ToolsR21."
+            self._set_dir()            # directories
+            self._set_input_fits()     # input maps
+            self._set_output_fits()    # output maps
+            self._set_input_param()    # input parameters
+            self._set_output_txt_png() # output txt and png
+
+    def _set_dir(self):
+        """
+        """
+
+        self.dir_proj     = self._read_key("dir_proj")
+        self.dir_raw      = self.dir_proj + self._read_key("dir_raw")
+        self.dir_cprops   = self.dir_proj + self._read_key("dir_cprops")
+        self.dir_env      = self.dir_proj + self._read_key("dir_env")
+        self.dir_piechart = self.dir_proj + self._read_key("dir_piechart")
+        self.dir_wise     = self.dir_proj + self._read_key("dir_wise")
+        self.dir_ready    = self.dir_proj + self._read_key("dir_ready")
+        self.dir_products = self.dir_proj + self._read_key("dir_products")
+        self.dir_final    = self.dir_proj + self._read_key("dir_final")
+
+        self._create_dir(self.dir_ready)
+        self._create_dir(self.dir_products)
+        self._create_dir(self.dir_final)
+
+    def _set_input_fits(self):
+        """
+        """
+
+        self.cube_co10_n0628 = self.dir_raw + self._read_key("cube_co10_n0628")
+        self.cube_co10_n3627 = self.dir_raw + self._read_key("cube_co10_n3627")
+        self.cube_co10_n4254 = self.dir_raw + self._read_key("cube_co10_n4254")
+        self.cube_co10_n4321 = self.dir_raw + self._read_key("cube_co10_n4321")
+
+        self.cube_co21_n0628 = self.dir_raw + self._read_key("cube_co21_n0628")
+        self.cube_co21_n3627 = self.dir_raw + self._read_key("cube_co21_n3627")
+        self.cube_co21_n4254 = self.dir_raw + self._read_key("cube_co21_n4254")
+        self.cube_co21_n4321 = self.dir_raw + self._read_key("cube_co21_n4321")
+
+        self.wise1_n0628     = self.dir_wise + self._read_key("wise1_n0628")
+        self.wise1_n3627     = self.dir_wise + self._read_key("wise1_n3627")
+        self.wise1_n4254     = self.dir_wise + self._read_key("wise1_n4254")
+        self.wise1_n4321     = self.dir_wise + self._read_key("wise1_n4321")
+
+        self.wise2_n0628     = self.dir_wise + self._read_key("wise2_n0628")
+        self.wise2_n3627     = self.dir_wise + self._read_key("wise2_n3627")
+        self.wise2_n4254     = self.dir_wise + self._read_key("wise2_n4254")
+        self.wise2_n4321     = self.dir_wise + self._read_key("wise2_n4321")
+
+        self.wise3_n0628     = self.dir_wise + self._read_key("wise3_n0628")
+        self.wise3_n3627     = self.dir_wise + self._read_key("wise3_n3627")
+        self.wise3_n4254     = self.dir_wise + self._read_key("wise3_n4254")
+        self.wise3_n4321     = self.dir_wise + self._read_key("wise3_n4321")
+
+    def _set_output_fits(self):
+        """
+        """
+
+        self.outcube_co10_n0628  = self.dir_ready + self._read_key("outcube_co10_n0628")
+        self.outcube_co10_n3627  = self.dir_ready + self._read_key("outcube_co10_n3627")
+        self.outcube_co10_n4254  = self.dir_ready + self._read_key("outcube_co10_n4254")
+        self.outcube_co10_n4321  = self.dir_ready + self._read_key("outcube_co10_n4321")
+
+        self.outcube_co21_n0628  = self.dir_ready + self._read_key("outcube_co21_n0628")
+        self.outcube_co21_n3627  = self.dir_ready + self._read_key("outcube_co21_n3627")
+        self.outcube_co21_n4254  = self.dir_ready + self._read_key("outcube_co21_n4254")
+        self.outcube_co21_n4321  = self.dir_ready + self._read_key("outcube_co21_n4321")
+
+        self.outfits_wise1_n0628 = self.dir_ready + self._read_key("outfits_wise1_n0628")
+        self.outfits_wise1_n3627 = self.dir_ready + self._read_key("outfits_wise1_n3627")
+        self.outfits_wise1_n4254 = self.dir_ready + self._read_key("outfits_wise1_n4254")
+        self.outfits_wise1_n4321 = self.dir_ready + self._read_key("outfits_wise1_n4321")
+
+        self.outfits_wise2_n0628 = self.dir_ready + self._read_key("outfits_wise2_n0628")
+        self.outfits_wise2_n3627 = self.dir_ready + self._read_key("outfits_wise2_n3627")
+        self.outfits_wise2_n4254 = self.dir_ready + self._read_key("outfits_wise2_n4254")
+        self.outfits_wise2_n4321 = self.dir_ready + self._read_key("outfits_wise2_n4321")
+
+        self.outfits_wise3_n0628 = self.dir_ready + self._read_key("outfits_wise3_n0628")
+        self.outfits_wise3_n3627 = self.dir_ready + self._read_key("outfits_wise3_n3627")
+        self.outfits_wise3_n4254 = self.dir_ready + self._read_key("outfits_wise3_n4254")
+        self.outfits_wise3_n4321 = self.dir_ready + self._read_key("outfits_wise3_n4321")
+
+    def _set_input_param(self):
+        """
+        """
+
+        self.ra_n0628  = self._read_key("ra_n0628", "gal").split("deg")[0]
+        self.ra_n3627  = self._read_key("ra_n3627", "gal").split("deg")[0]
+        self.ra_n4254  = self._read_key("ra_n4254", "gal").split("deg")[0]
+        self.ra_n4321  = self._read_key("ra_n4321", "gal").split("deg")[0]
+
+        self.dec_n0628 = self._read_key("dec_n0628", "gal").split("deg")[0]
+        self.dec_n3627 = self._read_key("dec_n3627", "gal").split("deg")[0]
+        self.dec_n4254 = self._read_key("dec_n4254", "gal").split("deg")[0]
+        self.dec_n4321 = self._read_key("dec_n4321", "gal").split("deg")[0]
+
+        self.basebeam_n0628 = float(self._read_key("basebeam_n0628"))
+        self.basebeam_n3627 = float(self._read_key("basebeam_n3627"))
+        self.basebeam_n4254 = float(self._read_key("basebeam_n4254"))
+        self.basebeam_n4321 = float(self._read_key("basebeam_n4321"))
+
+        self.imsize_n0628 = float(self._read_key("imsize_n0628"))
+        self.imsize_n3627 = float(self._read_key("imsize_n3627"))
+        self.imsize_n4254 = float(self._read_key("imsize_n4254"))
+        self.imsize_n4321 = float(self._read_key("imsize_n4321"))
+
+        self.chans_n0628 = self._read_key("chans_n0628")
+        self.chans_n3627 = self._read_key("chans_n3627")
+        self.chans_n4254 = self._read_key("chans_n4254")
+        self.chans_n4321 = self._read_key("chans_n4321")
+
+    def _set_output_txt_png(self):
+        """
+        """
+
+        # output txt and png
+        #self.outpng_mom0_13co10 = self.dir_products + self._read_key("outpng_mom0_13co10")
+
+    ####################
+    # run_phangs_ulirg #
+    ####################
+
+    def run_phangs_ulirg(
+        self,
+        do_all        = False,
+        # analysis
+        do_prepare    = False,
+        # plot figures in paper
+        plot_showcase = False,
+        # supplement
+        ):
+        """
+        This method runs all the methods which will create figures in the paper.
+        """
+
+        if do_all==True:
+            do_prepare = True
+
+        # analysis
+        if do_prepare==True:
+            self.align_cubes()
+            self.multismooth()
+
+        # plot figures in paper
+        #if plot_showcase==True:
+        #    self.showcase()
+
+    ###############
+    # multismooth #
+    ###############
+
+    def multismooth(
+        self,
+        ):
+        """
+        """
+
+        taskname = self.modname + sys._getframe().f_code.co_name
+        check_first(self.outcube_co10_n0628,taskname)
+
+        # line 710 of scripts_phangs_r21_tasks.py
+        outcube_template = self.outcube_co10_n0628.replace("04p0","????")
+
+    ###############
+    # align_cubes #
+    ###############
+
+    def align_cubes(
+        self,
+        ):
+        """
+        """
+
+        taskname = self.modname + sys._getframe().f_code.co_name
+        check_first(self.cube_co10_n0628,taskname)
+
+        self._align_cube_gal(self.cube_co10_n0628,self.cube_co21_n0628,
+            self.outcube_co10_n0628,self.outcube_co21_n0628,self.basebeam_n0628,
+            self.imsize_n0628,self.ra_n0628,self.dec_n0628,self.chans_n0628)
+
+        self._align_cube_gal(self.cube_co10_n3627,self.cube_co21_n3627,
+            self.outcube_co10_n3627,self.outcube_co21_n3627,self.basebeam_n3627,
+            self.imsize_n3627,self.ra_n3627,self.dec_n3627,self.chans_n3627)
+
+        self._align_cube_gal(self.cube_co10_n4254,self.cube_co21_n4254,
+            self.outcube_co10_n4254,self.outcube_co21_n4254,self.basebeam_n4254,
+            self.imsize_n4254,self.ra_n4254,self.dec_n4254,self.chans_n4254)
+
+        self._align_cube_gal(self.cube_co10_n4321,self.cube_co21_n4321,
+            self.outcube_co10_n4321,self.outcube_co21_n4321,self.basebeam_n4321,
+            self.imsize_n4321,self.ra_n4321,self.dec_n4321,self.chans_n4321)
+
+    ###################
+    # _align_cube_gal #
+    ###################
+
+    def _align_cube_gal(
+        self,
+        incube1,
+        incube2,
+        outcube1,
+        outcube2,
+        beam,
+        imsize,
+        ra,
+        dec,
+        chans,
+        ):
+        """
+        align_cubes
+        """
+
+        taskname = self.modname + sys._getframe().f_code.co_name
+        check_first(incube1,taskname)
+
+        # staging cubes
+        self._stage_cube(incube1,outcube1+"_tmp1",beam,imsize,ra,dec,115.27120)
+        self._stage_cube(incube2,outcube2+"_tmp1",beam,imsize,ra,dec,230.53800)
+
+        # align cubes
+        make_gridtemplate(outcube1+"_tmp1",outcube1+"_tmp2",imsize,ra,dec,beam)
+        print(outcube1+"_tmp2")
+        print(glob.glob(outcube1+"_tmp2"))
+        run_imregrid(outcube2+"_tmp1",outcube1+"_tmp2",outcube2+"_tmp1p5",
+            axes=[0,1])
+        os.system("rm -rf " + outcube2 + "_tmp1")
+        run_imregrid(outcube2+"_tmp1p5",outcube1+"_tmp2",outcube2+"_tmp2")
+        os.system("rm -rf " + outcube2 + "_tmp1p5")
+
+        # clip edge channels
+        run_immath_one(outcube1+"_tmp2",outcube1+"_tmp3","IM0",chans,delin=True)
+        run_immath_one(outcube2+"_tmp2",outcube2+"_tmp3","IM0",chans,delin=True)
+
+        # masking
+        run_immath_one(outcube1+"_tmp3",outcube1+"_tmp4","iif(IM0>-10000000.0,1,0)", "")
+        run_immath_one(outcube2+"_tmp3",outcube2+"_tmp4","iif(IM0>-10000000.0,1,0)", "")
+        run_immath_two(outcube1+"_tmp4",outcube2+"_tmp4",outcube1+"_combined_mask",
+            "IM0*IM1",delin=True)
+
+        run_immath_two(outcube1+"_tmp3",outcube1+"_combined_mask",outcube1+"_tmp4","iif(IM1>0,IM0,0)")
+        run_immath_two(outcube2+"_tmp3",outcube1+"_combined_mask",outcube2+"_tmp4","iif(IM1>0,IM0,0)",
+            delin=True)
+
+        imhead(outcube1+"_tmp4",mode="put",hdkey="beamminor",hdvalue=str(beam)+"arcsec")
+        imhead(outcube1+"_tmp4",mode="put",hdkey="beammajor",hdvalue=str(beam)+"arcsec")
+        imhead(outcube2+"_tmp4",mode="put",hdkey="beamminor",hdvalue=str(beam)+"arcsec")
+        imhead(outcube2+"_tmp4",mode="put",hdkey="beammajor",hdvalue=str(beam)+"arcsec")
+
+        unitconv_Jyb_K(outcube1+"_tmp4",outcube1,115.27120,unitto="Jy/beam",delin=True)
+        unitconv_Jyb_K(outcube2+"_tmp4",outcube2,230.53800,unitto="Jy/beam",delin=True)
+
+    ###############
+    # _stage_cube #
+    ###############
+
+    def _stage_cube(
+        self,
+        incube,
+        outcube,
+        beam,
+        imsize,
+        ra,
+        dec,
+        restfreq=115.27120,
+        ):
+        """
+        _align_cube_gal
+        """
+
+        taskname = self.modname + sys._getframe().f_code.co_name
+        check_first(incube,taskname)
+
+        run_importfits(incube,outcube+"_tmp1")
+        run_roundsmooth(outcube+"_tmp1",outcube+"_tmp2",
+            beam,delin=True)
+        unitconv_Jyb_K(outcube+"_tmp2",outcube+"_tmp3",restfreq,delin=True)
+        self._mask_fov_edges(outcube+"_tmp3",outcube+"_fovmask")
+        run_immath_two(outcube+"_tmp3",outcube+"_fovmask",outcube,
+            "iif(IM1>0,IM0,0)",delin=True)
+        imhead(outcube,mode="put",hdkey="beamminor",hdvalue=str(beam)+"arcsec")
+        imhead(outcube,mode="put",hdkey="beammajor",hdvalue=str(beam)+"arcsec")
+
+    ###################
+    # _mask_fov_edges #
+    ###################
+
+    def _mask_fov_edges(
+        self,
+        imagename,
+        outfile,
+        delin=False,
+        ):
+        """
+        _stage_cube
+        """
+
+        taskname = self.modname + sys._getframe().f_code.co_name
+        check_first(imagename,taskname)
+
+        expr1 = "iif(IM0>=-100000000., 1, 0)"
+        run_immath_one(imagename,imagename+"_mask_fov_edges_tmp1",expr1,"")
+        run_roundsmooth(imagename+"_mask_fov_edges_tmp1",
+            imagename+"_mask_fov_edges_tmp2",55.0,delin=True)
+
+        maxval = imstat(imagename+"_mask_fov_edges_tmp2")["max"][0]
+        expr2 = "iif(IM0>=" + str(maxval*0.6) + ", 1, 0)"
+        run_immath_one(imagename+"_mask_fov_edges_tmp2",
+            outfile,expr2,"",delin=True)
+        #boolean_masking(imagename+"_mask_fov_edges_tmp3",outfile,delin=True)
+
+    ###############
+    # _create_dir #
+    ###############
+
+    def _create_dir(self, this_dir):
+
+        if self.refresh==True:
+            print("## refresh " + this_dir)
+            os.system("rm -rf " + this_dir)
+
+        if not glob.glob(this_dir):
+            print("## create " + this_dir)
+            os.mkdir(this_dir)
+
+        else:
+            print("## not refresh " + this_dir)
+
+    #############
+    # _read_key #
+    #############
+
+    def _read_key(self, key, keyfile="fig", delimiter=",,,"):
+
+        if keyfile=="gal":
+            keyfile = self.keyfile_gal
+        elif keyfile=="fig":
+            keyfile = self.keyfile_fig
+
+        keydata  = np.loadtxt(keyfile,dtype="str",delimiter=delimiter)
+        keywords =\
+             np.array([s.replace(" ","") for s in keydata[:,0]])
+        values   = keydata[:,1]
+        value    = values[np.where(keywords==key)[0][0]]
+
+        return value
+
+#####################
+# end of ToolsULIRG #
+#####################
