2a57b508f230fa427201ca55bba6d9eae80ac4f1
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-10-17 19:42:22 +0900

    update phangs_r21

--- scripts_phangs_r21.py
+++ scripts_phangs_r21.py
@@ -429,45 +429,45 @@ class ToolsR21():
 
         list_rms_co10_n0628 = self._measure_log_rms(
             self.outcube_co10_n0628.replace(str(self.basebeam_n0628).replace(".","p").zfill(4),"????").replace(".image","_k.image"),
-            self.beams_n0628,
+            self.beams_n0628[:-2],
             self.noise_vs_beam_co10_n0628,
             )
         list_rms_co21_n0628 = self._measure_log_rms(
             self.outcube_co21_n0628.replace(str(self.basebeam_n0628).replace(".","p").zfill(4),"????").replace(".image","_k.image"),
-            self.beams_n0628,
+            self.beams_n0628[:-2],
             self.noise_vs_beam_co21_n0628,
             )
 
         list_rms_co10_n3627 = self._measure_log_rms(
             self.outcube_co10_n3627.replace(str(self.basebeam_n3627).replace(".","p").zfill(4),"????").replace(".image","_k.image"),
-            self.beams_n3627,
+            self.beams_n3627[:-2],
             self.noise_vs_beam_co10_n3627,
             )
         list_rms_co21_n3627 = self._measure_log_rms(
             self.outcube_co21_n3627.replace(str(self.basebeam_n3627).replace(".","p").zfill(4),"????").replace(".image","_k.image"),
-            self.beams_n3627,
+            self.beams_n3627[:-2],
             self.noise_vs_beam_co21_n3627,
             )
 
         list_rms_co10_n4254 = self._measure_log_rms(
             self.outcube_co10_n4254.replace(str(self.basebeam_n4254).replace(".","p").zfill(4),"????").replace(".image","_k.image"),
-            self.beams_n4254,
+            self.beams_n4254[:-2],
             self.noise_vs_beam_co10_n4254,
             )
         list_rms_co21_n4254 = self._measure_log_rms(
             self.outcube_co21_n4254.replace(str(self.basebeam_n4254).replace(".","p").zfill(4),"????").replace(".image","_k.image"),
-            self.beams_n4254,
+            self.beams_n4254[:-2],
             self.noise_vs_beam_co21_n4254,
             )
 
         list_rms_co10_n4321 = self._measure_log_rms(
             self.outcube_co10_n4321.replace(str(self.basebeam_n4321).replace(".","p").zfill(4),"????").replace(".image","_k.image"),
-            self.beams_n4321,
+            self.beams_n4321[:-2],
             self.noise_vs_beam_co10_n4321,
             )
         list_rms_co21_n4321 = self._measure_log_rms(
             self.outcube_co21_n4321.replace(str(self.basebeam_n4321).replace(".","p").zfill(4),"????").replace(".image","_k.image"),
-            self.beams_n4321,
+            self.beams_n4321[:-2],
             self.noise_vs_beam_co21_n4321,
             )
 
@@ -489,15 +489,15 @@ class ToolsR21():
         myax_set(ax, "both", xlim, ylim, title, xlabel, ylabel, adjust=ad)
 
         # plot co10 rms
-        ax.plot(self.beams_n0628, list_rms_co10_n0628[:,1], "o-", color=self.c_n0628, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 0628 CO(1-0)")
-        ax.plot(self.beams_n3627, list_rms_co10_n3627[:,1], "o-", color=self.c_n3627, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 3627 CO(1-0)")
-        ax.plot(self.beams_n4254, list_rms_co10_n4254[:,1], "o-", color=self.c_n4254, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 4254 CO(1-0)")
-        ax.plot(self.beams_n4321, list_rms_co10_n4321[:,1], "o-", color=self.c_n4321, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 4321 CO(1-0)")
+        ax.plot(self.beams_n0628[:-2], list_rms_co10_n0628[:,1], "o-", color=self.c_n0628, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 0628 CO(1-0)")
+        ax.plot(self.beams_n3627[:-2], list_rms_co10_n3627[:,1], "o-", color=self.c_n3627, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 3627 CO(1-0)")
+        ax.plot(self.beams_n4254[:-2], list_rms_co10_n4254[:,1], "o-", color=self.c_n4254, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 4254 CO(1-0)")
+        ax.plot(self.beams_n4321[:-2], list_rms_co10_n4321[:,1], "o-", color=self.c_n4321, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 4321 CO(1-0)")
         # plot co21 rms
-        ax.plot(self.beams_n0628, list_rms_co21_n0628[:,1], "s--", color=self.c_n0628, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 0628 CO(2-1)")
-        ax.plot(self.beams_n3627, list_rms_co21_n3627[:,1], "s--", color=self.c_n3627, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 3627 CO(2-1)")
-        ax.plot(self.beams_n4254, list_rms_co21_n4254[:,1], "s--", color=self.c_n4254, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 4254 CO(2-1)")
-        ax.plot(self.beams_n4321, list_rms_co21_n4321[:,1], "s--", color=self.c_n4321, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 4321 CO(2-1)")
+        ax.plot(self.beams_n0628[:-2], list_rms_co21_n0628[:,1], "s--", color=self.c_n0628, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 0628 CO(2-1)")
+        ax.plot(self.beams_n3627[:-2], list_rms_co21_n3627[:,1], "s--", color=self.c_n3627, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 3627 CO(2-1)")
+        ax.plot(self.beams_n4254[:-2], list_rms_co21_n4254[:,1], "s--", color=self.c_n4254, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 4254 CO(2-1)")
+        ax.plot(self.beams_n4321[:-2], list_rms_co21_n4321[:,1], "s--", color=self.c_n4321, markeredgewidth=0, markersize = 20, lw=3, label = "NGC 4321 CO(2-1)")
 
         # text
         t=ax.text(0.95, 0.93, "NGC 0628", color=self.c_n0628, horizontalalignment="right", transform=ax.transAxes, size=self.legend_fontsize, fontweight="bold")
