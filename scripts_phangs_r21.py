f81c064567234d5a6f5550d056daaa9df8cbeb52
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-10-19 12:00:24 +0900

    update phangs_r21

--- scripts_phangs_r21.py
+++ scripts_phangs_r21.py
@@ -497,7 +497,7 @@ class ToolsR21():
             self._import_violins(this_co10,this_co21,this_r21,this_er21,this_beams_n0628,this_ra,this_dec,this_scale,this_pa,this_incl)
 
         this_basebeam = str(self.basebeam_n3627).replace(".","p").zfill(4)
-        this_beams_n0628 = [s for s in self.beams_n3627 if s%4==0]
+        this_beams_n3627 = [s for s in self.beams_n3627 if s%4==0]
         this_r21      = self.outfits_r21_n3627.replace(this_basebeam,"????")
         this_er21     = self.outfits_er21_n3627.replace(this_basebeam,"????")
         this_co10     = self.outmom_co10_n3627.replace(this_basebeam,"????").replace("momX","mom0")
@@ -511,7 +511,7 @@ class ToolsR21():
             self._import_violins(this_co10,this_co21,this_r21,this_er21,this_beams_n0628,this_ra,this_dec,this_scale,this_pa,this_incl)
 
         this_basebeam = str(self.basebeam_n4254).replace(".","p").zfill(4)
-        this_beams_n0628 = [s for s in self.beams_n4254 if s%4==0]
+        this_beams_n4254 = [s for s in self.beams_n4254 if s%4==0]
         this_r21      = self.outfits_r21_n4254.replace(this_basebeam,"????")
         this_er21     = self.outfits_er21_n4254.replace(this_basebeam,"????")
         this_co10     = self.outmom_co10_n4254.replace(this_basebeam,"????").replace("momX","mom0")
@@ -525,7 +525,7 @@ class ToolsR21():
             self._import_violins(this_co10,this_co21,this_r21,this_er21,this_beams_n0628,this_ra,this_dec,this_scale,this_pa,this_incl)
 
         this_basebeam = str(self.basebeam_n4321).replace(".","p").zfill(4)
-        this_beams_n0628 = [s for s in self.beams_n4321 if s%4==0]
+        this_beams_n4321 = [s for s in self.beams_n4321 if s%4==0]
         this_r21      = self.outfits_r21_n4321.replace(this_basebeam,"????")
         this_er21     = self.outfits_er21_n4321.replace(this_basebeam,"????")
         this_co10     = self.outmom_co10_n4321.replace(this_basebeam,"????").replace("momX","mom0")
