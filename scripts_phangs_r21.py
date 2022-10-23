1488654dd6a7c495e20f7bb4cd2e8c3305586166
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-10-23 22:07:03 +0900

    update phangs_r21

--- scripts_phangs_r21.py
+++ scripts_phangs_r21.py
@@ -738,7 +738,7 @@ class ToolsR21():
                 this_cut        = np.where((mod_co21>=nbins[i]) & (mod_co21<nbins[i+1]))
                 this_modsn_co10 = modsn_co10[this_cut]
                 this_mod_co21   = mod_co21[this_cut]
-                this_mods_co21  = np.log10(10**mod_co21[this_cut] + np.random.normal(0.0, np.sqrt((10**this_obserr)**2+(10**this_scatter)**2), len(mod_co21[this_cut])))
+                this_mods_co21  = np.log10(10**mod_co21[this_cut] + np.random.normal(0.0, 10**this_obserr, len(mod_co21[this_cut])))
                 modsn_co10_final.extend(this_modsn_co10)
                 mods_co21_final.extend(this_mods_co21)
                 mod_co21_final.extend(this_mod_co21)
@@ -746,7 +746,7 @@ class ToolsR21():
                 if len(this_mods_co21)>0:
                     nbins_available+=1
 
-            mods_co21_final = np.array(mods_co21_final)
+            mods_co21_final   = np.log10(10**np.array(mods_co21_final) + np.random.normal(0.0, 10**this_scatter, len(mods_co21_final)))
             nmodsn_co10_final = np.array(modsn_co10_final)
             best_chi2 = 1e44
             #this_chi2 = self._calc_chi2(obs_co21,mods_co21_final)
