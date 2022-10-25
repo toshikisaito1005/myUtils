87b14f05487afa7819f7171a0da3535d1e2cf654
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-10-25 10:57:16 +0900

    update phangs_r21

--- scripts_phangs_r21.py
+++ scripts_phangs_r21.py
@@ -568,7 +568,7 @@ class ToolsR21():
         """
 
         for i, this_beam in enumerate(beams):
-            if i!=2:
+            if i!=0:
                 continue
             this_beamstr    = str(this_beam).replace(".","p").zfill(4)
             this_txt        = inputtxt.replace("????",this_beamstr)
