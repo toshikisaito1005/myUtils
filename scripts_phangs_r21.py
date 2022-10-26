e27bfa4b8a57f560ff315722682fe9b8938f290a
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-10-26 10:37:15 +0900

    update phangs_r21

--- scripts_phangs_r21.py
+++ scripts_phangs_r21.py
@@ -843,9 +843,9 @@ class ToolsR21():
         """
 
         nbins         = int( (np.ceil(np.log2(len(obs))) + 1) + 1.5 ) * 2
-        range_scatter = [0.0, 1.0]
-        range_slope   = [slope-0.001, slope+0.001]
-        range_icept   = [icept-0.001, icept+0.001]
+        range_scatter = [0.0, 2.0]
+        range_slope   = [slope-0.3, slope+0.3]
+        range_icept   = [icept-0.3, icept+0.3]
 
         return [nbins, range_scatter, range_slope, range_icept]
 
