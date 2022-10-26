227aa9667ca6ed1061bb41b8ac73d9c848b30df3
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-10-26 20:40:11 +0900

    update phangs_r21

--- scripts_phangs_r21.py
+++ scripts_phangs_r21.py
@@ -570,8 +570,8 @@ class ToolsR21():
         start = time.time()
 
         for i, this_beam in enumerate(beams):
-            if i!=0:
-                continue
+            #if i!=0:
+            #    continue
             this_beamstr    = str(this_beam).replace(".","p").zfill(4)
             this_txt        = inputtxt.replace("????",this_beamstr)
             data            = np.loadtxt(this_txt)
