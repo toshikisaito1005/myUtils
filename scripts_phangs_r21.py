a5c6c96d3aa263ddcb356c4c0d45f499b975af4e
toshikisaito1005 <toshiki.saito1005@gmail.com>
2022-11-03 11:33:36 +0900

    update phangs_r21

--- scripts_phangs_r21.py
+++ scripts_phangs_r21.py
@@ -424,7 +424,7 @@ class ToolsR21():
 
         # final
         self.box_map_co10     = "1900x2100+290+190"
-        self.box_map_co21     = "1600x2100+590+190"
+        self.box_map_co21     = "1700x2100+490+190"
         self.final_mom0_n4321 = self.dir_final + self._read_key("final_mom0_n4321")
 
     ##################
