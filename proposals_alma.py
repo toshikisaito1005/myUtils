0477ecf568fb805d350322258fdb2db9cea33ec5
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-01-23 21:43:22 +0900

    update for cycle 10 proposal

--- proposals_alma.py
+++ proposals_alma.py
@@ -185,7 +185,7 @@ class ProposalsALMA():
         os.system("rm -rf template.image")
         template = "template.image"
         run_importfits(self.image_12co10,template,defaultaxes=True,defaultaxesvalues=["RA","Dec","1GHz","Stokes"])
-        run_importfits(self.image_oiiioii,self.image_oiiioii+"_tmp1",defaultaxes=True,defaultaxesvalues=["RA","Dec"])
+        run_importfits(self.image_oiiioii,self.image_oiiioii+"_tmp1")
         run_imregrid(self.image_13co10,          template, self.image_13co10+"_regrid",  delin=False)
         run_imregrid(self.image_ch3oh,           template, self.image_ch3oh+"_regrid",   delin=False)
         run_imregrid(self.image_h13cn,           template, self.image_h13cn+"_regrid",   delin=False)
