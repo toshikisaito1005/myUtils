7c2d343da1394653eb529d92a39c557924bfb84d
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-01-26 10:50:11 +0900

    update LST sim

--- scripts_lst_alma_spm.py
+++ scripts_lst_alma_spm.py
@@ -195,7 +195,7 @@ class ToolsLSTSpMSim():
             project     = self.project_gmaur+"_12m_"+totaltimetint,
             totaltime   = totaltime,
             incenter    = self.incenter,
-            pointingspacing = "0.1arcsec",
+            pointingspacing = "20arcsec",
             )
 
         run_simobserve(
@@ -205,7 +205,7 @@ class ToolsLSTSpMSim():
             project     = self.project_gmaur+"_12m_lst_"+totaltimetint,
             totaltime   = totaltime,
             incenter    = self.incenter,
-            pointingspacing = "0.1arcsec",
+            pointingspacing = "20arcsec",
             )
 
     #############################
