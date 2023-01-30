aed9887d0efb2dfccd31498afc54508939fc258d
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-01-30 14:04:39 +0900

    update PHANGS R21

--- scripts_phangs_r21.py
+++ scripts_phangs_r21.py
@@ -937,6 +937,23 @@ class ToolsR21():
 
         this_basebeam = str(self.basebeam_n0628).replace(".","p").zfill(4)
         this_params   = self.outtxt_mod_n0628.replace(this_basebeam,"*").replace("_model","_param")
+
+        this_basebeam = str(self.basebeam_n3672).replace(".","p").zfill(4)
+        this_params   = self.outtxt_mod_n3672.replace(this_basebeam,"*").replace("_model","_param")
+        this_files    = glob.glob(this_params)
+
+        for this_file in this_files:
+            print(np.loadtxt(this_file)[0])
+
+        this_basebeam = str(self.basebeam_n4254).replace(".","p").zfill(4)
+        this_params   = self.outtxt_mod_n4254.replace(this_basebeam,"*").replace("_model","_param")
+        this_files    = glob.glob(this_params)
+
+        for this_file in this_files:
+            print(np.loadtxt(this_file)[0])
+
+        this_basebeam = str(self.basebeam_n4321).replace(".","p").zfill(4)
+        this_params   = self.outtxt_mod_n4321.replace(this_basebeam,"*").replace("_model","_param")
         this_files    = glob.glob(this_params)
 
         for this_file in this_files:
