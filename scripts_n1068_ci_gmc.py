5baa49f2933c77f2ef77d6fb2ec332767bd547ad
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-09-11 17:49:16 +0900

    update ci-gmc

--- scripts_n1068_ci_gmc.py
+++ scripts_n1068_ci_gmc.py
@@ -385,14 +385,14 @@ class ToolsCIGMC():
         ndata   = ndata["data"].flatten()
         data    = data[data/ndata>-10000]
 
-        histx, histy, histrange, peak, rms, x_bestfit, y_bestfit, _ = self._gaussfit_noise(data,bins=500)
+        histx, histy, histrange, peak, rms, x_bestfit, y_bestfit, _ = self._gaussfit_noise(data,bins=1000)
 
         xlim     = [0, 10*rms]
         ylim     = [0, np.max(histy)*2.02]
         title    = "CO(1-0) Cube"
         xlabel   = "Absolute voxel value (K)"
         ylabel   = "Count"
-        binwidth = (histrange[1]-histrange[0]) / 500.
+        binwidth = (histrange[1]-histrange[0]) / 1000.
         c_pos    = "tomato"
         c_neg    = "deepskyblue"
 
