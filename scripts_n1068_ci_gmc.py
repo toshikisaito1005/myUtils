fab2471b11ea5a4ddfca2a12e6009c8bbece9a4a
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-09-14 13:16:59 +0900

    update ci-gmc

--- scripts_n1068_ci_gmc.py
+++ scripts_n1068_ci_gmc.py
@@ -500,7 +500,11 @@ class ToolsCIGMC():
         ax1.plot([np.log10(55),np.log10(55)], ylim, linestyle='dashed', color='grey', alpha=.5, lw=2)
 
         # const surface density lines (e.g., Leroy+15)
-        Cnst = np.log10((np.pi*6.674*10**-11/5.)**0.5*(10**-3)**1.5*(5.0279*10**-31)**0.5*(3.086*10**13)**-1*(3.086*10**13)**0.5)
+        G = 6.674*10**-11
+        m2km = 10**-3
+        kg2msun = 5.02785e-31
+        pc2kms = 3.086*10**13
+        Cnst = np.log10((np.pi*G/5.)**0.5*m2km**1.5*kg2msun**-0.5*pc2kms**-1*pc2kms**0.5)
         y_285 = [0.5*np.log10(285)+0.5*xlim[0]+Cnst, 0.5*np.log10(285)+0.5*xlim[1]+Cnst]
 
         print(xlim, y_285)
