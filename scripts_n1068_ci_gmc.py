7813c5b855811b1e849fda7f7d023a401d820289
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-09-14 18:47:38 +0900

    update ci-gmc

--- scripts_n1068_ci_gmc.py
+++ scripts_n1068_ci_gmc.py
@@ -519,7 +519,7 @@ class ToolsCIGMC():
 
         # scatterhist
         self._scatter_hist(x_co10, y_co10, ax1, ax2, ax3, "deepskyblue", xlim, ylim, "s")
-        self._scatter_hist(x_ci10, y_ci10, ax1, ax2, ax3, "tomato", xlim, ylim, offset=0.3)
+        self._scatter_hist(x_ci10, y_ci10, ax1, ax2, ax3, "tomato", xlim, ylim, offset=0.2)
 
         # scatter for outflow data
         ax1.scatter(x_co10_cone, y_co10_cone, c="deepskyblue", lw=2, s=100, marker="s")
@@ -601,7 +601,7 @@ class ToolsCIGMC():
         ax_histx.text(0.02, 0.85, "16$^{th}$-50$^{th}$-84$^{th}$ pctls.", color="black", transform=ax_histx.transAxes, fontsize=16)
 
         this_txt = str(int(10**np.percentile(x,16)))+"-"+str(int(10**np.percentile(x,50)))+"-"+str(int(10**np.percentile(x,84)))+" (pc)"
-        ax_histx.text(0.02, 0.55-offset, this_txt, color=color, transform=ax_histx.transAxes, weight="bold", fontsize=16)
+        ax_histx.text(0.02, 0.65-offset, this_txt, color=color, transform=ax_histx.transAxes, fontsize=16)
 
     ###############
     # hist_cprops #
