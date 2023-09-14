b2b72a8d79a3b586a84725e31c836781ec97f8bb
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-09-14 19:45:38 +0900

    update ci-gmc

--- scripts_n1068_ci_gmc.py
+++ scripts_n1068_ci_gmc.py
@@ -704,7 +704,7 @@ class ToolsCIGMC():
 
         # stats of xhist
         ax_histx.text(0.74, 0.85, "16$^{th}$-50$^{th}$-84$^{th}$ pctls.", color="black", transform=ax_histx.transAxes, fontsize=15)
-        this_txt = str(int(10**np.percentile(x,16)))+"-"+str(int(10**np.percentile(x,50)))+"-"+str(int(10**np.percentile(x,84)))+" ($M_{\odot}$ pc$^{-2}$)"
+        this_txt = str(int(10**np.percentile(x,16)))+"-"+str(int(10**np.percentile(x,50)))+"-"+str(int(10**np.percentile(x,84)))
         ax_histx.text(0.74, 0.70-offset, this_txt, color=color, transform=ax_histx.transAxes, fontsize=15)
 
         # stats of yhist
