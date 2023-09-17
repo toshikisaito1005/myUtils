0e6e8ba70ff506dc07844f249975fa8c66a9f16a
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-09-17 17:21:30 +0900

    update ci-gmc

--- scripts_n1068_ci_gmc.py
+++ scripts_n1068_ci_gmc.py
@@ -406,12 +406,22 @@ class ToolsCIGMC():
         f,_ = imval_all(self.ncube_ci10)
         ndata_ci10 = f["data"].flatten()
 
-        this_co10  = data_co10[mask_ci10==0]
-        this_nco10 = ndata_co10[mask_ci10==0]
-        this_ci10  = data_ci10[mask_ci10==0]
-        this_nci10 = ndata_ci10[mask_ci10==0]
+        for i in range(len(cnum_co10)):
+            this_co10  = data_co10[mask_ci10==i]
+            this_nco10 = ndata_co10[mask_ci10==i]
+            this_ci10  = data_ci10[mask_ci10==i]
+            this_nci10 = ndata_ci10[mask_ci10==i]
 
-        print(np.nanmean(this_ci10/this_co10))
+            mask = np.where((this_co10>=this_nco10*3) & (this_ci10>=this_nci10*3))
+            this_co10   = np.sum(this_co10[mask])
+            this_nco10  = np.sqrt(np.sum((this_nco10[mask])**2))
+            this_ci10   = np.sum(this_ci10[mask])
+            this_nci10  = np.sqrt(np.sum((this_nci10[mask])**2))
+
+            this_ratio  = this_ci10 / this_co10
+            this_nratio = this_ratio * np.sqrt((this_nco10/this_co10)**2 + (this_nci10/this_ci10)**2)
+
+            print(i, np.round(this_ratio,2), np.round(this_nratio,2))
 
     ###############
     # plot_larson #
