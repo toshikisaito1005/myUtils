43956ee38760139d027eef1a381e12bc95e78a01
toshikisaito1005 <toshiki.saito1005@gmail.com>
2023-09-28 17:09:29 +0900

    update ci-gmc

--- scripts_n1068_ci_gmc.py
+++ scripts_n1068_ci_gmc.py
@@ -441,10 +441,10 @@ class ToolsCIGMC():
         cut = np.where((mom0_co10>emom0_co10*self.snr_mom) & (mom2_co10>emom2_co10))
         x_co10     = x_co10[cut]
         y_co10     = y_co10[cut]
-        emom0_co10 = emom0_co10[cut] / mom0_co10[cut] / np.log(10)
-        mom0_co10  = np.log10(mom0_co10[cut])
-        emom2_co10 = emom2_co10[cut] / mom2_co10[cut] / np.log(10)
-        mom2_co10  = np.log10(mom2_co10[cut])
+        emom0_co10 = emom0_co10[cut] #/ mom0_co10[cut] / np.log(10)
+        mom0_co10  = mom0_co10[cut] # np.log10(mom0_co10[cut])
+        emom2_co10 = emom2_co10[cut] #/ mom2_co10[cut] / np.log(10)
+        mom2_co10  = mom2_co10[cut] # np.log10(mom2_co10[cut])
 
         # import ci10
         data_ci10 = np.loadtxt(self.outtxt_hexcat_ci10)
@@ -459,18 +459,18 @@ class ToolsCIGMC():
         cut = np.where((mom0_ci10>emom0_ci10*self.snr_mom) & (mom2_ci10>emom2_ci10))
         x_ci10     = x_ci10[cut]
         y_ci10     = y_ci10[cut]
-        emom0_ci10 = emom0_ci10[cut] / mom0_ci10[cut] / np.log(10)
-        mom0_ci10  = np.log10(mom0_ci10[cut])
-        emom2_ci10 = emom2_ci10[cut] / mom2_ci10[cut] / np.log(10)
-        mom2_ci10  = np.log10(mom2_ci10[cut])
+        emom0_ci10 = emom0_ci10[cut] #/ mom0_ci10[cut] / np.log(10)
+        mom0_ci10  = mom0_ci10[cut] # np.log10(mom0_ci10[cut])
+        emom2_ci10 = emom2_ci10[cut] #/ mom2_ci10[cut] / np.log(10)
+        mom2_ci10  = mom2_ci10[cut] # np.log10(mom2_ci10[cut])
 
         ########
         # plot #
         ########
         x_co10 = mom0_co10
-        y_co10 = emom2_co10
+        y_co10 = mom2_co10
         x_ci10 = mom0_ci10
-        y_ci10 = emom2_ci10
+        y_ci10 = mom2_ci10
 
         xlim   = [np.min([np.nanmin(x_co10),np.nanmin(x_ci10)])-0.2,np.max([np.nanmax(x_co10),np.nanmax(x_ci10)])+0.2]
         ylim   = [np.min([np.nanmin(y_co10),np.nanmin(y_ci10)])-0.2,np.max([np.nanmax(y_co10),np.nanmax(y_ci10)])+0.2]
